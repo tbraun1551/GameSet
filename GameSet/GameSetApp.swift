4673264c791cb2514fae3c1c6338731ea96ec73c
Thomas braun <tbraun1551@gmail.com>
2020-06-24 14:22:16 -0500

    Initial Commit

---
+++ GameSet/GameSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSetApp.swift
+//  GameSet
+//
+//  Created by Thomas Braun on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct GameSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
